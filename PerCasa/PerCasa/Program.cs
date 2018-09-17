1ebdfc647922a22fec632c689a22517a9b23c12c
Robert Constantin Coman <coman.robert.constantin@gmail.com>
2018-09-17 15:25:07 +0200

    RovaCasa

---
+++ PerCasa/PerCasa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerCasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 55555;
+            Console.WriteLine($"Il valore di a è {a}");
+            short s = (short)a;
+            Console.WriteLine($"Il valore di s è {s}");
+            Console.ReadLine();
+        }
+    }
+}
